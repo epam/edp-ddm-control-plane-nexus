6fbc8e12ab3d01569bc9f01f9b61760316027aed
Oleksandr Lazebnyi <oleksandr_lazebnyi@epam.com>
2023-04-06 12:31:17 +0300

    [EDP-DDM-25184] Fixed restart of cp-nexus
    Change-Id: Ife5dc6aa29917a6f7024700797dc7dadcb62514f

--- scripts/dc_patching.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#Patching for namespace
-oc patch -n $1 deployment/nexus --type json -p '[{"op": "replace", "path": "/spec/template/spec/containers/0/ports/0", "value": {"name": "docker", "protocol": "TCP", "containerPort": 5000}}, {"op": "replace", "path": "/spec/template/spec/containers/0/ports/0", "value": {"protocol": "TCP", "containerPort": 8443}}, {"op": "replace", "path": "/spec/template/spec/containers/0/env/0/value", "value": "nexus"}]'
-
-
